f63c31493e5d36f9231aacfe0e5f30ca7f5776a9
mkk <mk.kyaw@mit.com.mm>
2023-09-19 03:03:43 +0630

    search responsive

--- components/header.tsx
+++ components/header.tsx
@@ -14,14 +14,14 @@ function Header() {
   return (
     <header className=''>
       <div className='bg-[#DFF6FF] text-sm'>
-        <div className='max-w-screen-xl mx-auto flex items-center justify-between p-2 '>
+        <div className='max-w-screen-xl mx-auto flex flex-col md:flex-row text-left md:items-center justify-between p-2 '>
           <p>+959 7980 65880</p>
           <p>UPDATES – Click Here for KBZ Money News Alerts – UPDATES</p>
           <p>admin@kbzmoney.com</p>
         </div>
       </div>
       <nav className='max-w-screen-xl mx-auto bg-white border-gray-200 dark:bg-gray-900'>
-        <div className=' flex flex-wrap items-center justify-between  p-4'>
+        <div className=' flex flex-wrap items-center justify-between md:justify-normal  p-4'>
           <a href='/' className='flex items-center'>
             <Image
               height={50}
@@ -31,15 +31,15 @@ function Header() {
               alt='Comp Logo'
             />
           </a>
-          <div className='flex md:order-2'>
-            <div className={`mr-3 ${searchOpen ? "block" : "hidden"}`}>
-              <Input className='w-full' placeholder='Search' />
-            </div>
+          <div className='flex md:order-2  md:ml-auto'>
             <Button
               className='rounded-lg'
               variant='ghost'
               size='icon'
-              onClick={() => setSearchOpen((prev) => !prev)}
+              onClick={() => {
+                setSearchOpen((prev) => !prev);
+                setMenuOpen((prev) => !prev);
+              }}
             >
               <SearchIcon />
             </Button>
@@ -47,24 +47,34 @@ function Header() {
               className='rounded-lg md:hidden'
               variant='ghost'
               size='icon'
-              onClick={() => setMenuOpen((prev) => !prev)}
+              onClick={() => {
+                setMenuOpen((prev) => !prev);
+                setSearchOpen((prev) => !prev);
+              }}
             >
               <AlignRightIcon />
             </Button>
           </div>
           <div
-            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+            className={`md:ml-28 items-center justify-between w-full md:flex md:w-[67%] md:order-1 ${
               menuOpen ? "block" : "hidden"
             }`}
             id='navbar-search'
           >
+            <div
+              className={`md:ml-auto md:order-1 mt-5 md:mt-0 ${
+                searchOpen ? "block" : "hidden"
+              }`}
+            >
+              <Input className='w-full' placeholder='Search' />
+            </div>
             <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
               <li>
                 <a
                   href='/'
-                  className={`block py-2 pl-3 pr-4 rounded  md:p-0 md:dark:text-blue-500 ${
+                  className={`block py-2 pl-3 pr-4 rounded  md:p-0 md:dark:text-[#015EB9] ${
                     pathname === "/"
-                      ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700"
+                      ? "text-white bg-[#015EB9] md:bg-transparent md:text-[#015EB9]"
                       : "text-gray-900"
                   }`}
                   aria-current='page'
@@ -75,9 +85,9 @@ function Header() {
               <li>
                 <a
                   href='/blog'
-                  className={`block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${
+                  className={`block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#015EB9] md:p-0 md:dark:hover:text-[#015EB9] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${
                     pathname.startsWith("/blog")
-                      ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700"
+                      ? "text-white bg-[#015EB9] md:bg-transparent md:text-[#015EB9]"
                       : "text-gray-900"
                   }`}
                 >
